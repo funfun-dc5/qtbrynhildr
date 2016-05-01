8e7a273dcbf31737a12ed5c7126dbcb52dd534d1
FunFun <fu.aba.dc5@gmail.com>
2016-05-01 15:53:07 +0900

    fixed for v126

--- src/version.h
+++ src/version.h
@@ -6,9 +6,9 @@
 #define VERSION_H
 
 // version
-#define QTB_VERSION				"1.2.5"
+#define QTB_VERSION				"1.2.6"
 #define QTB_RCNAME				""
-#define QTB_VERSION_NUMBER		125
+#define QTB_VERSION_NUMBER		126
 
 // year
 #define QTB_YEAR		"2015-2016"

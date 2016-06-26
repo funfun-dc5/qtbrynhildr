2a944d20c1af0d78fba6c1fdd13b0aeac3344ffd
FunFun <fu.aba.dc5@gmail.com>
2016-06-26 20:42:06 +0900

    fixed for v155

--- src/version.h
+++ src/version.h
@@ -6,9 +6,9 @@
 #define VERSION_H
 
 // version
-#define QTB_VERSION				"1.5.4"
+#define QTB_VERSION				"1.5.5"
 #define QTB_RCNAME				""
-#define QTB_VERSION_NUMBER		154
+#define QTB_VERSION_NUMBER		155
 
 // year
 #define QTB_YEAR		"2015-2016"

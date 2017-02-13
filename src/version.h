891de0638dc777a5116249635cdb2a4ecc8629bc
FunFun <fu.aba.dc5@gmail.com>
2017-02-13 14:31:04 +0900

    fixed for v163

--- src/version.h
+++ src/version.h
@@ -5,9 +5,9 @@
 #define VERSION_H
 
 // version
-#define QTB_VERSION				"1.6.2"
-#define QTB_RCNAME				"-android"
-#define QTB_VERSION_NUMBER		162
+#define QTB_VERSION				"1.6.3"
+#define QTB_RCNAME				""
+#define QTB_VERSION_NUMBER		163
 
 // year
 #define QTB_YEAR		"2015-2017"

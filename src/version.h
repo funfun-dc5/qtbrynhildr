920f2721649dcd01a1548bc9157d868c012648d6
FunFun <fu.aba.dc5@gmail.com>
2018-02-10 00:54:39 +0900

    fixed for v2012

--- src/version.h
+++ src/version.h
@@ -5,9 +5,9 @@
 #define VERSION_H
 
 // version
-#define QTB_VERSION				"2.0.11"
-#define QTB_RCNAME				"-rpi3"
-#define QTB_VERSION_NUMBER		2011
+#define QTB_VERSION				"2.0.12"
+#define QTB_RCNAME				""
+#define QTB_VERSION_NUMBER		2012
 
 // year
 #define QTB_YEAR		"2015-2018"

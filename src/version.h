b951c1fa8661a5ba7b9c7e82a4b11d55c7a445aa
FunFun <fu.aba.dc5@gmail.com>
2018-02-23 09:42:48 +0900

    fixed for v2014

--- src/version.h
+++ src/version.h
@@ -5,9 +5,9 @@
 #define VERSION_H
 
 // version
-#define QTB_VERSION				"2.0.13"
-#define QTB_RCNAME				"-android"
-#define QTB_VERSION_NUMBER		2013
+#define QTB_VERSION				"2.0.14"
+#define QTB_RCNAME				""
+#define QTB_VERSION_NUMBER		2014
 
 // year
 #define QTB_YEAR		"2015-2018"

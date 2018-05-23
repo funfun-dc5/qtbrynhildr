e44407a1bfb026d81e52bdfc8bd06de5688c32e4
FunFun <fu.aba.dc5@gmail.com>
2018-05-23 19:45:56 +0900

    updated copyright

--- doc/manual/source/conf.py
+++ doc/manual/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Qt Brynhildr Manual'
-copyright = u'2015, FunFun @ mcz-xoxo'
+copyright = u'2015-2018, FunFun @ mcz-xoxo'
 author = u'FunFun @ mcz-xoxo'
 
 # The version info for the project you're documenting, acts as replacement for

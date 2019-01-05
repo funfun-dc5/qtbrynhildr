7b35082b95a8db119a40b9b0ee3e6c9dcaee145b
FunFun <fu.aba.dc5@gmail.com>
2019-01-05 20:17:28 +0900

    fixed for WIN64

--- src/version.h
+++ src/version.h
@@ -9,11 +9,11 @@
 #define QTB_RCNAME				""
 #define QTB_VERSION_NUMBER		2021
 
-#if __x86_64__ || __LP64__
+#if __x86_64__ || __LP64__ || _WIN64
 #define QTB_ARCHNAME			" (64bit)"
-#else // __x86_64__ || __LP64__
+#else // __x86_64__ || __LP64__ || _WIN64
 #define QTB_ARCHNAME			" (32bit)"
-#endif // __x86_64__ || __LP64__
+#endif // __x86_64__ || __LP64__ || _WIN64
 
 // year
 #define QTB_YEAR		"2015-2018"

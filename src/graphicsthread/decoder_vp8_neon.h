a760d8680d581080c0b0ddc9f73472727d347ea6
FunFun <fu.aba.dc5@gmail.com>
2020-02-14 21:10:07 +0900

    added DecorderVP8NEON class

---
+++ src/graphicsthread/decoder_vp8_neon.h
@@ -0,0 +1,59 @@
+// -*- mode: c++; coding: utf-8-unix -*-
+// Copyright (c) 2020 FunFun <fu.aba.dc5@gmail.com>
+
+#ifndef DECODER_VP8_NEON_H
+#define DECODER_VP8_NEON_H
+// Common Header
+#include "common/common.h"
+
+// System Header
+
+// Qt Header
+#include <QImage>
+
+// Local Header
+#include "decoder_vp8.h"
+
+namespace qtbrynhildr {
+
+class DecoderVP8NEON : public DecoderVP8
+{
+  //-------------------------------------------------------------------------------
+  // Variable
+  //-------------------------------------------------------------------------------
+private:
+  // output log flag
+  bool outputLog;
+
+  //-------------------------------------------------------------------------------
+  // Function
+  //-------------------------------------------------------------------------------
+public:
+  // constructor
+  DecoderVP8NEON();
+  // destructor
+  virtual ~DecoderVP8NEON();
+
+  // get decoder name
+  const char*  name()
+  {
+	return "vp8 sse decoder";
+  }
+
+#if 0
+  // pre-process
+  bool preprocess(char *buffer, int size);
+#endif // 0
+
+  // get desktop image
+  QImage* getDesktopImage(int numOfThread);
+
+protected:
+
+private:
+  
+};
+
+} // end of namespace qtbrynhildr
+
+#endif // DECODER_VP8_NEON_H

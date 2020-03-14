e4ad1ebad4385e60d56e3c7554c9fc328999cbbd
FunFun <fu.aba.dc5@gmail.com>
2020-03-14 10:51:52 +0900

    fixed for v2029-rc1

--- src/version.h
+++ src/version.h
@@ -5,10 +5,10 @@
 #define VERSION_H
 
 // version
-#define QTB_VERSION				"2.0.28"
+#define QTB_VERSION				"2.0.29"
 
-#define QTB_RCNAME				"-branch-mousewheel"
-#define QTB_VERSION_NUMBER		2028
+#define QTB_RCNAME				"-rc1"
+#define QTB_VERSION_NUMBER		2029
 
 #if __x86_64__ || __LP64__ || _WIN64
 #define QTB_ARCHNAME			" (64bit)"

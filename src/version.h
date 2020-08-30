00d01b1db7388007606b0ec1070e0a28bce6c7d2
FunFun <fu.aba.dc5@gmail.com>
2020-08-30 20:24:01 +0900

    fixed for v2099

--- src/version.h
+++ src/version.h
@@ -5,10 +5,10 @@
 #define VERSION_H
 
 // version
-#define QTB_VERSION				"2.0.98"
+#define QTB_VERSION				"2.0.99"
 
-#define QTB_RCNAME				"-branch-android"
-#define QTB_VERSION_NUMBER		2098
+#define QTB_RCNAME				""
+#define QTB_VERSION_NUMBER		2099
 
 #if __x86_64__ || __LP64__ || _WIN64
 #define QTB_ARCHNAME			" (64bit)"

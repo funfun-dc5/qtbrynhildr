71ca8a1216c72f62515593732e0219ca20155fbc
FunFun <fu.aba.dc5@gmail.com>
2020-10-21 13:48:49 +0900

    fixed for v218

--- src/version.h
+++ src/version.h
@@ -5,10 +5,10 @@
 #define VERSION_H
 
 // version
-#define QTB_VERSION				"2.1.7"
+#define QTB_VERSION				"2.1.8"
 
 #define QTB_RCNAME				""
-#define QTB_VERSION_NUMBER		2107
+#define QTB_VERSION_NUMBER		2108
 
 #if __x86_64__ || __LP64__ || _WIN64
 #define QTB_ARCHNAME			" (64bit)"

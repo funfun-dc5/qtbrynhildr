5499333422a98b555e2edec972e8136622d38c83
FunFun <fu.aba.dc5@gmail.com>
2021-02-22 10:12:31 +0900

    fixed for v233-rc1

--- src/version.h
+++ src/version.h
@@ -5,10 +5,10 @@
 #define VERSION_H
 
 // version
-#define QTB_VERSION				"2.3.2"
+#define QTB_VERSION				"2.3.3"
 
-#define QTB_RCNAME				""
-#define QTB_VERSION_NUMBER		2302
+#define QTB_RCNAME				"-rc1"
+#define QTB_VERSION_NUMBER		2303
 
 #if __x86_64__ || __LP64__ || _WIN64
 #define QTB_ARCHNAME			" (64bit)"

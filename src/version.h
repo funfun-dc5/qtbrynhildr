2ea08ac4e0779e3062049dddc395e738a6391427
FunFun <fu.aba.dc5@gmail.com>
2022-02-14 11:55:18 +0900

    fixed for v241

--- src/version.h
+++ src/version.h
@@ -7,8 +7,8 @@
 // version
 #define QTB_VERSION				"2.4.1"
 
-#define QTB_RCNAME				"-rc1"
-#define QTB_VERSION_NUMBER		2400
+#define QTB_RCNAME				""
+#define QTB_VERSION_NUMBER		2401
 
 #if __x86_64__ || __LP64__ || _WIN64
 #define QTB_ARCHNAME			" (64bit)"

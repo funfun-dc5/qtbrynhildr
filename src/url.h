41f997b586a70302da64e7e7721ce0700f2826f8
FunFun <fu.aba.dc5@gmail.com>
2023-01-11 20:13:16 +0900

    updated Privacy Pollicy (and about dialog)

--- src/url.h
+++ src/url.h
@@ -11,6 +11,9 @@
 //#define QTB_MANUAL		"<a href=\"https://mcz-xoxo.net/qtbrynhildr/docs/manual/html/\">https://mcz-xoxo.net/qtbrynhildr/docs/manual/html/</a>"
 #define QTB_MANUAL		"<a href=\"https://mcz-xoxo.net/qtbrynhildr/docs/manual/html/\">Ja</a>"
 
+// URL of privacy pollicy
+#define QTB_PRIVACY_POLICY		"<a href=\"https://mcz-xoxo.net/qtbrynhildr/docs/privacy/English.html\">En</a>"
+
 // URL of release page in GitHub
 #define QTB_URL_FOR_RELEASE		"https://github.com/funfun-dc5/qtbrynhildr/releases"
 
